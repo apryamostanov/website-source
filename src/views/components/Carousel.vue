8445aa4eda8a7056221b69c7ee829dba64aab1fe
apryamostanov <anton.pryamostanov@gmail.com>
2020-05-25 17:24:51 +0300

    Progress

--- src/views/components/Carousel.vue
+++ src/views/components/Carousel.vue
@@ -11,11 +11,28 @@
         <div class="container py-0 pb-lg">
             <div class="row justify-content-between align-items-center">
                 <div class="col-lg-5 mb-5 mb-lg-0">
-                    <h1 class="text-white font-weight-light">Mission</h1>
-                    <p class="lead text-white mt-4"><b>Infinite Technology ∞</b> is an open-source software foundation established in 2018.</p>
-                    <p class="lead text-white mt-4">Our mission is to streamline Web and Mobile app development by standardizing and systemizing the primary infrastructure components.</p>
-                    <a href="https://demos.creative-tim.com/argon-design-system/docs/components/alerts.html"
-                       class="btn btn-white mt-4">See more</a>
+
+                    <div class="d-flex px-3">
+                        <div>
+                            <icon name="ni ni-world-2" type="info" rounded class="mb-4 text-white-50 shadow ">
+                            </icon>
+                        </div>
+
+                        <div class="pl-4">
+                            <h4 class="display-3 text-white">Mission</h4>
+                        </div>
+                    </div>
+
+                    <div class="d-flex px-3">
+                        <div class="pl-0">
+                            <p class="lead text-white mt-4"><b>Infinite Technology ∞</b> is an open-source software
+                                foundation established in 2018.</p>
+                            <p class="lead text-white mt-4">Our mission is to streamline Web and Mobile app development
+                                by standardizing and systemizing the primary infrastructure components.</p>
+                            <a href="https://demos.creative-tim.com/argon-design-system/docs/components/alerts.html"
+                               class="btn btn-white mt-4">See more</a>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-lg-6 mb-lg-auto">
                     <div class="rounded shadow-lg overflow-hidden transform-perspective-right">
@@ -38,15 +55,15 @@
     </section>
 </template>
 <script>
-import { BCarousel } from "bootstrap-vue/esm/components/carousel/carousel";
-import { BCarouselSlide } from "bootstrap-vue/esm/components/carousel/carousel-slide";
+    import {BCarousel} from "bootstrap-vue/esm/components/carousel/carousel";
+    import {BCarouselSlide} from "bootstrap-vue/esm/components/carousel/carousel-slide";
 
-export default {
-  components: {
-    BCarousel,
-    BCarouselSlide
-  }
-};
+    export default {
+        components: {
+            BCarousel,
+            BCarouselSlide
+        }
+    };
 </script>
 <style>
 </style>
