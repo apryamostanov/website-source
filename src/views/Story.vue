a5f2e02b810a0fb6c77827a8d24fbec97af1325e
apryamostanov <anton.pryamostanov@gmail.com>
2020-07-13 17:34:22 +0300

    Progress

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="section section section-shaped my-0 overflow-hidden">
         <div>
-            <div class="shape shape-style-1 shape-primary bg-gradient-green">
+            <div class="shape shape-style-1 shape-primary bg-gradient-indigo">
                 <span></span>
                 <span></span>
                 <span></span>
@@ -12,8 +12,8 @@
             <div class="container pt-100">
                 <div class="row justify-content-center text-center mb-sm">
                     <div class="col-lg-8">
-                        <h2 class="display-3 text-white">Pioneers of Mobile Banking</h2>
-                        <p class="lead text-white">More than a decade of Fintech Innovation</p>
+                        <h2 class="display-3 text-white">Our Story</h2>
+                        <p class="lead text-white">More than a decade of Fintech Innovation.</p>
                     </div>
                 </div>
                 <card shadow class="shadow-lg--hover mt-5">
