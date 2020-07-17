fbeef45846c868fa5d8578b38d1a23453e4a5e04
apryamostanov <anton.pryamostanov@gmail.com>
2020-07-17 12:56:20 +0300

    Progress

--- src/views/components/Products.vue
+++ src/views/components/Products.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="section section section-shaped my-0 overflow-hidden">
+    <section class="section section section-shaped my-0 overflow-hidden pb-3">
         <div class="shape shape-style-1 purple-gradient">
             <span></span>
             <span></span>
@@ -11,7 +11,7 @@
         <div class="container py-0">
             <div class="d-flex px-3">
                 <div>
-                    <icon name="fa fa-cubes" size="lg" type="white" color="q" shadow
+                    <icon name="fa fa-cubes" size="lg" type="white" color="primary" shadow
                           rounded></icon>
                 </div>
                 <div class="pl-4">
@@ -19,112 +19,120 @@
                     <p class="lead text-white">Open Source. Free. Financial grade.</p>
                 </div>
             </div>
-            <div class="row row-grid-1 align-items-center">
-                <div class="col-lg-6 order-lg-1">
-                    <card shadow class="shadow-lg--hover mt-5">
-                        <div class="d-flex px-3">
-                            <div>
-
-                                <div class="icon icon-shape bg-transparent rounded-circle text-white"
-                                     style=" padding: 0">
-                                    <img src="img/theme/Bobbin_small.png" alt="Circle image"
-                                         class="img-fluid rounded-circle ">
+            <div class="container mt-5">
+                <div class="row row-grid-1">
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="pb-5">
+                            <card class="border-0" shadow body-classes="py-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="icon icon-shape bg-transparent rounded-circle text-white"
+                                         style=" padding: 0">
+                                        <img src="img/theme/Bobbin_small.png" alt="Circle image"
+                                             class="img-fluid rounded-circle ">
+                                    </div>
+                                    <div class="pl-2 ">
+                                        <h6 class=" text-uppercase mb-0">Bobbin</h6>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="pl-4">
-                                <h5 class="title text-success">Bobbin</h5>
-                                <p>Revolutionary SLF4J logger.</p>
-                                <base-button class="btn-1" outline type="success">See More</base-button>
-                            </div>
+                                <p class="description mt-3">Revolutionary SLF4J logger.</p>
+                                <router-link to="/ZZZ" class="btn btn-info mt-2">See More</router-link>
+                            </card>
                         </div>
-                    </card>
-                    <card shadow class="shadow-lg--hover mt-5">
-                        <div class="d-flex px-3">
-                            <div>
-
-                                <div class="icon icon-shape bg-transparent rounded-circle text-white"
-                                     style=" padding: 0">
-                                    <img src="img/theme/Pigeon_small.png" alt="Circle image"
-                                         class="img-fluid rounded-circle ">
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="pb-5">
+                            <card class="border-0" shadow body-classes="py-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="icon icon-shape bg-transparent rounded-circle text-white"
+                                         style=" padding: 0">
+                                        <img src="img/theme/BlackBox_small.png" alt="Circle image"
+                                             class="img-fluid rounded-circle ">
+                                    </div>
+                                    <div class="pl-2 ">
+                                        <h6 class=" text-uppercase mb-0">BlackBox</h6>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="pl-4">
-                                <h5 class="title text-success">Pigeon</h5>
-                                <p>HTTP Message Broker.</p>
-                                <base-button class="btn-1" outline type="success">See More</base-button>
-                            </div>
+                                <p class="description mt-3">Groovy logging AST.</p>
+                                <router-link to="/ZZZ" class="btn btn-info mt-2">See More</router-link>
+                            </card>
                         </div>
-                    </card>
-                    <card shadow class="shadow-lg--hover mt-5">
-                        <div class="d-flex px-3">
-                            <div>
-
-                                <div class="icon icon-shape bg-transparent rounded-circle text-white"
-                                     style=" padding: 0">
-                                    <img src="img/theme/COBOL_small.png" alt="Circle image"
-                                         class="img-fluid rounded-circle ">
-                                </div>
-                            </div>
-                            <div class="pl-4">
-                                <h5 class="title text-success">COBOL ETL</h5>
-                                <p>Java COBOL Transpiler and ETL API.</p>
-                                <base-button class="btn-1" outline type="success">See More</base-button>
-                            </div>
-                        </div>
-                    </card>
+                    </div>
                 </div>
-                <div class="col-lg-6 order-lg-2">
-                    <card shadow class="shadow-lg--hover mt-5">
-                        <div class="d-flex px-3">
-                            <div>
-
-                                <div class="icon icon-shape bg-transparent rounded-circle text-white"
-                                     style=" padding: 0">
-                                    <img src="img/theme/BlackBox_small.png" alt="Circle image"
-                                         class="img-fluid rounded-circle ">
+                <div class="row row-grid-1">
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="pb-5">
+                            <card class="border-0" shadow body-classes="py-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="icon icon-shape bg-transparent rounded-circle text-white"
+                                         style=" padding: 0">
+                                        <img src="img/theme/Pigeon_small.png" alt="Circle image"
+                                             class="img-fluid rounded-circle ">
+                                    </div>
+                                    <div class="pl-2 ">
+                                        <h6 class=" text-uppercase mb-0">Pigeon</h6>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="pl-4">
-                                <h5 class="title text-success">BlackBox</h5>
-                                <p>Groovy logging AST.</p>
-                                <base-button class="btn-1" outline type="success">See More</base-button>
-                            </div>
+                                <p class="description mt-3">HTTP Message Broker.</p>
+                                <router-link to="/ZZZ" class="btn btn-info mt-2">See More</router-link>
+                            </card>
                         </div>
-                    </card>
-                    <card shadow class="shadow-lg--hover mt-5">
-                        <div class="d-flex px-3">
-                            <div>
-
-                                <div class="icon icon-shape bg-transparent rounded-circle text-white"
-                                     style=" padding: 0">
-                                    <img src="img/theme/David_small.png" alt="Circle image"
-                                         class="img-fluid rounded-circle ">
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="pb-5">
+                            <card class="border-0" shadow body-classes="py-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="icon icon-shape bg-transparent rounded-circle text-white"
+                                         style=" padding: 0">
+                                        <img src="img/theme/David_small.png" alt="Circle image"
+                                             class="img-fluid rounded-circle ">
+                                    </div>
+                                    <div class="pl-2 ">
+                                        <h6 class=" text-uppercase mb-0">David</h6>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="pl-4">
-                                <h5 class="title text-success">David</h5>
-                                <p>Telegram Chat Bot.</p>
-                                <base-button class="btn-1" outline type="success">See More</base-button>
-                            </div>
+                                <p class="description mt-3">Telegram Chat Bot.</p>
+                                <router-link to="/ZZZ" class="btn btn-info mt-2">See More</router-link>
+                            </card>
                         </div>
-                    </card>
-                    <card shadow class="shadow-lg--hover mt-5">
-                        <div class="d-flex px-3">
-                            <div>
-
-                                <div class="icon icon-shape bg-transparent rounded-circle text-white"
-                                     style=" padding: 0">
-                                    <img src="img/theme/Ascend_small.png" alt="Circle image"
-                                         class="img-fluid rounded-circle ">
+                    </div>
+                </div>
+                <div class="row row-grid-1">
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="pb-5">
+                            <card class="border-0" shadow body-classes="py-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="icon icon-shape bg-transparent rounded-circle text-white"
+                                         style=" padding: 0">
+                                        <img src="img/theme/COBOL_small.png" alt="Circle image"
+                                             class="img-fluid rounded-circle ">
+                                    </div>
+                                    <div class="pl-2 ">
+                                        <h6 class=" text-uppercase mb-0">COBOL ETL</h6>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="pl-4">
-                                <h5 class="title text-success">Ascend</h5>
-                                <p>Web Security IAM and SECaaS.</p>
-                                <base-button class="btn-1" outline type="success">See More</base-button>
-                            </div>
+                                <p class="description mt-3">Java COBOL Transpiler and ETL API.</p>
+                                <router-link to="/ZZZ" class="btn btn-info mt-2">See More</router-link>
+                            </card>
                         </div>
-                    </card>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="pb-5">
+                            <card class="border-0" shadow body-classes="py-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="icon icon-shape bg-transparent rounded-circle text-white"
+                                         style=" padding: 0">
+                                        <img src="img/theme/Ascend_small.png" alt="Circle image"
+                                             class="img-fluid rounded-circle ">
+                                    </div>
+                                    <div class="pl-2 ">
+                                        <h6 class=" text-uppercase mb-0">Ascend</h6>
+                                    </div>
+                                </div>
+                                <p class="description mt-3">Identity and Access Management.</p>
+                                <router-link to="/ZZZ" class="btn btn-info mt-2">See More</router-link>
+                            </card>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
